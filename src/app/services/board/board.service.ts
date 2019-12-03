6cf4e999fa4081a3bf0f3e2630899db96c83b4c8
Burak Yılmaz <byilmaz.work@gmail.com>
2019-12-03 16:35:16 +0300

    Fixed refresh issue. MineCount added to the status bar.

--- src/app/services/board/board.service.ts
+++ src/app/services/board/board.service.ts
@@ -3,15 +3,15 @@ import { Injectable } from '@angular/core';
 import { BoardOptions } from 'src/app/interfaces/board-options.interface';
 
 const PEERS = [
-    [-1, -1],
-    [-1, 0],
-    [-1, 1],
-    [0, -1],
-    [0, 0],
-    [0, 1],
-    [1, -1],
-    [1, 0],
-    [1, 1],
+  [-1, -1],
+  [-1, 0],
+  [-1, 1],
+  [0, -1],
+  [0, 0],
+  [0, 1],
+  [1, -1],
+  [1, 0],
+  [1, 1],
 ];
 
 
@@ -19,96 +19,100 @@ const PEERS = [
   providedIn: 'root',
 })
 export class Board {
-    private defaultOptions: BoardOptions = {
-      size: 15,
-      mines: 50,
-    };
+  private defaultOptions: BoardOptions = {
+    size: 15,
+    mines: 30,
+  };
 
-    cells: Cell[][] = [];
+  cells: Cell[][] = [];
 
-    private remainingCells = 0;
-    private mineCount = 0;
-    constructor(){
-      this.refresh();
+  private remainingCells = 0;
+  private mineCount = 0;
+  constructor() {
+    this.refresh();
+  }
+
+  refresh(options?: BoardOptions) {
+    const size = options ? options.size || this.defaultOptions.size : this.defaultOptions.size;
+    const mines = options ? options.mines || this.defaultOptions.mines : this.defaultOptions.mines;
+
+    this.remainingCells = 0;
+    this.mineCount = 0;
+
+    for (let y = 0; y < size; y++) {
+      this.cells[y] = [];
+      for (let x = 0; x < size; x++) {
+        this.cells[y][x] = new Cell(y, x);
+      }
     }
 
-    refresh(options?: BoardOptions) {
-      const size = options ? options.size || this.defaultOptions.size : this.defaultOptions.size;
-      const mines = options ? options.mines || this.defaultOptions.mines: this.defaultOptions.mines;;
-        for (let y = 0; y < size; y++) {
-            this.cells[y] = [];
-            for (let x = 0; x < size; x++) {
-                this.cells[y][x] = new Cell(y, x);
-            }
+    // Assign Mines
+    for (let i = 0; i < mines; i++) {
+      this.getRandomCell().mine = true;
+    }
+
+    // Count Mines
+    for (let y = 0; y < size; y++) {
+      for (let x = 0; x < size; x++) {
+        let adjacentMines = 0;
+        for (const peer of PEERS) {
+          if (this.cells[y + peer[0]] && this.cells[y + peer[0]][x + peer[1]] && this.cells[y + peer[0]][x + peer[1]].mine) {
+            adjacentMines++;
+          }
         }
+        this.cells[y][x].proximityMines = adjacentMines;
 
-        // Assign Mines
-        for (let i = 0; i < mines; i++) {
-            this.getRandomCell().mine = true;
+        if (this.cells[y][x].mine) {
+          this.mineCount++;
         }
+      }
+    }
+    this.remainingCells = (size * size) - this.mineCount;
+  }
 
-        // Count Mines
-        for (let y = 0; y < size; y++) {
-            for (let x = 0; x < size; x++) {
-                let adjacentMines = 0;
-                for (const peer of PEERS) {
-                    if(this.cells[y+peer[0]] && this.cells[y+peer[0]][x+peer[1]] && this.cells[y+peer[0]][x+peer[1]].mine) {
-                        adjacentMines++;
-                    }
-                }
-                this.cells[y][x].proximityMines = adjacentMines;
+  getRandomCell(): Cell {
+    let y = Math.floor(Math.random() * this.cells.length);
+    let x = Math.floor(Math.random() * this.cells[y].length);
 
-                if(this.cells[y][x].mine){
-                    this.mineCount++;
-                }
-            }
+    while(this.cells[y][x].mine){
+      y = Math.floor(Math.random() * this.cells.length);
+      x = Math.floor(Math.random() * this.cells[y].length);
+    }
+
+    return this.cells[y][x];
+  }
+
+  checkCell(cell: Cell): 'gameover' | 'win' | null {
+    if (cell.status !== "open") {
+      return;
+    } else if (cell.mine) {
+      this.revealAll();
+      return 'gameover';
+    } else {
+      cell.status = 'clear';
+      //Empty celli let's clear the whole block.
+      if (cell.proximityMines === 0) {
+        for (const peer of PEERS) {
+          if (this.cells[cell.row + peer[0]] && this.cells[cell.row + peer[0]][cell.column + peer[1]]) {
+            this.checkCell(this.cells[cell.row + peer[0]][cell.column + peer[1]]);
+          }
         }
-        this.remainingCells = (size * size) - this.mineCount;
-
+      }
+      if (this.remainingCells-- <= 1) {
+        return 'win';
+      };
+      return;
     }
+  }
 
-    getMineCount(): number {
-      console.log("MineCount");
-      return this.mineCount;
-    }
-
-    getRandomCell(): Cell {
-        const y = Math.floor(Math.random() * this.cells.length);
-        const x = Math.floor(Math.random() * this.cells[y].length);
-        return this.cells[y][x];
-    }
-
-    checkCell(cell: Cell): 'gameover' | 'win' | null {
-        if(cell.status !== "open") {
-            return;
-        } else if(cell.mine) {
-            this.revealAll();
-            return 'gameover';
-        } else {
-            cell.status = 'clear';
-            //Empty celli let's clear the whole block.
-            if(cell.proximityMines === 0) {
-                for(const peer of PEERS) {
-                    if(this.cells[cell.row + peer[0]] && this.cells[cell.row + peer[0]][cell.column + peer[1]]) {
-                        this.checkCell(this.cells[cell.row + peer[0]][cell.column + peer[1]]);
-                    }
-                }
-            }
-            if(this.remainingCells-- <= 1) {
-                return 'win';
-            };
-            return;
+  revealAll() {
+    for (const row of this.cells) {
+      for (const cell of row) {
+        if (cell.status === 'open') {
+          cell.status = 'clear';
         }
+      }
     }
+  }
 
-    revealAll(){
-        for (const row of this.cells) {
-            for (const cell of row) {
-                if(cell.status === 'open') {
-                    cell.status = 'clear';
-                }
-            }
-        }
-    }
-  
 }
